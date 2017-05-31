92bc3d994a49531acf7c429b82faa701a077cf9a
Mehul <mehul.makwana@accedo.tv>
2017-05-31 15:29:51 +0800

    push

--- env-local.sh
+++ env-local.sh
@@ -2,5 +2,5 @@
 
 export NODE_ENV='development'
 export BASE_URL='http://localhost:3000'
-export RELAYME_DATABASE_URL='localhost:27017/relayme'
+export RELAYME_DATABASE_URL='mongodb://ci:0cF3GC0wIOHt@ds127391.mlab.com:27391/relayme-ci'
 
